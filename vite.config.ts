e6b2a35a9ea2b8030739744d73c99ef7cc4cc5eb
sakthisk14 <155798137+sakthisk14@users.noreply.github.com>
2025-12-24 16:25:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import tailwind from "tailwindcss";
+import { defineConfig } from "vite";
+import path from "path";
 
-export default defineConfig(({ mode }) => {
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwind()],
+    },
+  },
 });
